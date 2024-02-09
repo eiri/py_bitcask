98f3a1e11ffb1cb81a6c3650000d4bab0c881cdd
Eric Avdey <eiri@eiri.ca>
2024-02-09 19:17:04 -0400

    refactor: less ad-hoc'y implementation for put/get

--- src/py_bitcask/bitcask.py
+++ src/py_bitcask/bitcask.py
@@ -1,6 +1,11 @@
 import binascii
+import ctypes
 
 from uuid_extensions import uuid7
+
+UINT_SZ = ctypes.sizeof(ctypes.c_uint)
+ULONG_SZ = ctypes.sizeof(ctypes.c_ulong)
+FILE_ID_SIZE = 1
 
 
 class Singleton(type):
@@ -15,6 +20,9 @@ class Singleton(type):
 
 
 class Bitcask(metaclass=Singleton):
+    S_VAL = slice(FILE_ID_SIZE, FILE_ID_SIZE + ULONG_SZ)
+    S_POS = slice(FILE_ID_SIZE + ULONG_SZ, FILE_ID_SIZE + 2 * ULONG_SZ)
+
     def __init__(self):
         self.cur = 0
         self.keydir = {}
@@ -26,23 +34,28 @@ class Bitcask(metaclass=Singleton):
     def get(self, key):
         if key not in self.keydir:
             raise KeyError
-        key_block = self.keydir[key]
-        value_sz = int.from_bytes(key_block[1:5])
-        value_pos = int.from_bytes(key_block[5:9])
-        return self.data[value_pos : value_pos + value_sz]
+        value_pos_bin = self.keydir[key][self.S_POS]
+        value_pos = int.from_bytes(value_pos_bin)
+        value_sz_bin = self.keydir[key][self.S_VAL]
+        value_sz = int.from_bytes(value_sz_bin)
+        s_value = slice(value_pos, value_pos + value_sz)
+        return self.data[s_value]
 
     def put(self, key, value):
         tstamp = uuid7().bytes
-        val_len = len(value)
         block = bytes(
-            tstamp + len(key).to_bytes(4) + val_len.to_bytes(4) + key + value
+            tstamp
+            + len(key).to_bytes(ULONG_SZ)
+            + len(value).to_bytes(ULONG_SZ)
+            + key
+            + value
         )
-        self.data += binascii.crc32(block).to_bytes(4) + block
-        self.cur += 4 + len(block)
+        self.data += binascii.crc32(block).to_bytes(UINT_SZ) + block
+        self.cur += UINT_SZ + len(block)
         self.keydir[key] = bytes(
             b"0"
-            + len(value).to_bytes(4)
-            + (self.cur - val_len).to_bytes(4)
+            + len(value).to_bytes(ULONG_SZ)
+            + (self.cur - len(value)).to_bytes(ULONG_SZ)
             + tstamp
         )
         return True
